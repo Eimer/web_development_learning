aed2be6a15509fad9fbf99e1d768b157f4195607
Yaroslav Pratsovity <ypratsovit@e2r6p5.unit.ua>
2020-08-22 14:36:33 +0300

    finished task t05

--- sprint02/t04/js/script.js
+++ sprint02/t04/js/script.js
@@ -1,8 +1,14 @@
 function solver(a, b, c, d, e) {
+    let x;
+
     if (!a || !b || !c || !d || !e)
         return("Wrong input");
-    else
-        return (a * a) - (5 * b * c) + (d / 3) + e;
+    else if (typeof(a) != "number" || typeof(b) != "number" 
+        || typeof(c) != "number" || typeof(d) != "number" 
+        || typeof(e) != "number")
+        return("Wrong input");
+    else {
+        x = (a * a) - (5 * b * c) + (d / 3) + e;
+        return x.toFixed(2);
+    }        
 }
-
-alert(solver(1,2,3,4,5));
---
+++ sprint02/t06/js/script.js
@@ -0,0 +1,9 @@
+const num = 2;
+const bigI = 2n;
+const str = "hello";
+const bool = true;
+const noth = null;
+const undef = undefined;
+
+alert(`num is ${typeof(num)}\n` + `bigI is ${typeof(bigI)}\n` + 
+        )
