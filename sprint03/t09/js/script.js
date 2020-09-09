26d0585820029cc86565148ef43810903a7f8b1d
Yaroslav Pratsovity <ypratsovit@e1r10p10.unit.ua>
2020-09-09 13:49:55 +0300

    finished task t09

--- sprint03/t09/js/script.js
+++ sprint03/t09/js/script.js
@@ -1,19 +1,13 @@
-//Prototype class of mark (single grid)
-class Mark {
-  constructor(elem) {
-    this.value = "";
-    this.elem = elem;
-  }
-  render(value) {
+
+let Mark = {
+  value: "",
+  elem: undefined,
+  render: function(elem, value) {
     this.value = value;
-    this.elem.innerHTML = this.value;
-  }
-  clear() {
-    this.value = "";
-    this.elem.innerHTML = this.value;
+    elem.innerHTML = this.value;
   }
 }
-//Object of service (count turns, restart option and other)
+
 let service = {
     turnsCount: 0,
     isPlayerOne: true,
@@ -55,35 +49,109 @@ let service = {
         }
         turns.innerHTML = this.turnsCount;
     },
-    restart: function() {
+    restart: function(grids) {
         let btn = document.getElementsByClassName("restart-btn");
+
         btn[0].addEventListener('click', function () {
+
+          let grids = document.getElementsByClassName("grids");
+            for (let i = 0; i < 9; i ++) {
+              grids[0].children[i].children[0].innerHTML = "";
+              grids[0].children[i].children[0].style.color = "rgb(50, 50, 50)";
+            }
+            service.endOfgame("");
             service.clear();
             service.render();
         });
+    },
+    endOfgame: function (value) {
+      let playerOne = this.getPlyerOne();
+      let playerTwo = this.getPlyerTwo();
+      let getElem = document.getElementsByClassName("winner");
+
+      getElem[0].innerHTML = value;
+      getElem[0].style.color = "rgb(0, 160, 0)";
+      playerOne.style.background = "rgb(0, 160, 0)";
+      playerTwo.style.background = "rgb(0, 160, 0)";
+    },
+    coloring: function (elem, pos1, pos2, pos3) {
+      elem[pos1].children[0].style.color = "rgb(0, 160, 0)";
+      elem[pos2].children[0].style.color = "rgb(0, 160, 0)";
+      elem[pos3].children[0].style.color = "rgb(0, 160, 0)";
     }
 }
 
-service.changeTurn();
-service.render();
-service.restart();
-
-function createMarksArr() {
-  let arrayOfMarks = [];
-  let arrayOfgrids = document.getElementsByClassName("one-grid");
-
-  for (let i = 0; i < 9; i++)
-    arrayOfMarks[i] = new Mark(arrayOfgrids[i].children[0], "");
-    console.log(arrayOfMarks);
+function game() {
+  let grids = document.getElementsByClassName("grids");
+  let getWinner = document.getElementsByClassName("winner");
+  function setMark (event) {
+    if (event.target != grids[0] && event.target.innerHTML == "" 
+      && getWinner[0].innerHTML == "") {
+      if (service.isPlayerOne) {
+        Mark.render(event.target, "X");
+        service.changeTurn();
+      }
+      else if (service.isPlayerTwo) {
+        Mark.render(event.target, "O");
+        service.changeTurn();
+      }
+      service.render();
+      let gridValue = document.getElementsByClassName("one-grid");
+        for (let i = 0; i < 9; i++) {
+          if (i < 7 && i % 3 == 0 && gridValue[i].children[0].innerHTML == "X"
+            && gridValue[i + 1].children[0].innerHTML == "X"
+            && gridValue[i + 2].children[0].innerHTML == "X") {
+              service.endOfgame("Player 1 won!");
+              service.coloring(gridValue, i, i + 1, i + 2);
+            }
+          else if (i < 7 && i % 3 == 0 && gridValue[i].children[0].innerHTML == "O"
+            && gridValue[i + 1].children[0].innerHTML == "O"
+            && gridValue[i + 2].children[0].innerHTML == "O") {
+              service.endOfgame("Player 2 won!");
+              service.coloring(gridValue, i, i + 1, i + 2);
+            }
+          else if (i < 3 && gridValue[i].children[0].innerHTML == "X"
+            && gridValue[i + 3].children[0].innerHTML == "X"
+            && gridValue[i + 6].children[0].innerHTML == "X") {
+              service.endOfgame("Player 1 won!");
+              service.coloring(gridValue, i, i + 3, i + 6);
+            }
+          else if (i < 3 && gridValue[i].children[0].innerHTML == "O"
+            && gridValue[i + 3].children[0].innerHTML == "O"
+            && gridValue[i + 6].children[0].innerHTML == "O") {
+              service.endOfgame("Player 2 won!");
+              service.coloring(gridValue, i, i + 3, i + 6);
+            }
+          else if (i == 0 && gridValue[i].children[0].innerHTML == "X"
+            && gridValue[i + 4].children[0].innerHTML == "X"
+            && gridValue[i + 8].children[0].innerHTML == "X") {
+              service.endOfgame("Player 1 won!");
+              service.coloring(gridValue, i, i + 4, i + 8);
+            }
+          else if (i == 0 && gridValue[i].children[0].innerHTML == "O"
+            && gridValue[i + 4].children[0].innerHTML == "O"
+            && gridValue[i + 8].children[0].innerHTML == "O") {
+              service.endOfgame("Player 2 won!");
+              service.coloring(gridValue, i, i + 4, i + 8);
+            }
+          else if (i == 2 && gridValue[i].children[0].innerHTML == "X"
+            && gridValue[i + 2].children[0].innerHTML == "X"
+            && gridValue[i + 4].children[0].innerHTML == "X") {
+              service.endOfgame("Player 1 won!");
+              service.coloring(gridValue, i, i + 2, i + 4);
+            }
+          else if (i == 2 && gridValue[i].children[0].innerHTML == "O"
+            && gridValue[i + 2].children[0].innerHTML == "O"
+            && gridValue[i + 4].children[0].innerHTML == "O") {
+              service.endOfgame("Player 2 won!");
+              service.coloring(gridValue, i, i + 2, i + 4);
+            }
+          else if (service.turnsCount == 9)
+              service.endOfgame("It`s a draw!");
+        }
+    }
+    service.restart();
+  }
+  grids[0].addEventListener("click", setMark);
 }
-
-createMarksArr();
-
-
-// let test = document.getElementsByClassName("grids");
-// test[0].addEventListener("click", showTest);
-// console.log(test[0]);
-
-// function showTest (event) {
-//   console.log(event.target.children[0]);
-// }
+game();
